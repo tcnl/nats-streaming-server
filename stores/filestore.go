bf907d8b859b8def7b8d68a5df4f40a70cf0cf64
GabrielBarbosaGV <gbgv@cin.ufpe.br>
2019-06-10 10:02:47 -0300

    Completed

--- stores/filestore.go
+++ stores/filestore.go
@@ -2892,9 +2892,9 @@ func (ms *FileMsgStore) enforceLimits(reportHitLimit, lockFile bool) error {
 			ms.log.Warnf(droppingMsgsFmt, ms.subject, ms.totalCount, ms.limits.MaxMsgs,
 				util.FriendlyBytes(int64(ms.totalBytes)), util.FriendlyBytes(ms.limits.MaxBytes))
 		}
-	}
 
-	time.Sleep(time.Second * 5)
+		time.Sleep(time.Second * 1)
+	}
 
 	EnforcingLimits = false
 
